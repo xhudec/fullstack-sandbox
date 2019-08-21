647b964edfa2966705f72e30f2d511dc4405ca1c
Lukas Hudec <hudec.luke@gmail.com>
2019-08-21 22:27:39 +0200

    feat: simple Hello World FE and BE apps

---
+++ frontend/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <div>
+        <h1>Welcome to Next.js!</h1>
+      </div>
+    </div>
+  );
+}
